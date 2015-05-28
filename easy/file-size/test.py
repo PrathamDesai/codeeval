8177051bdb91d6503c38147a46dd2b5bec2d172b
thiagoalessio <thiagoalessio@me.com>
2015-05-28 12:01:50 +0200

    file-size solution implemented in python

---
+++ easy/file-size/test.py
@@ -0,0 +1,11 @@
+import unittest
+import sys
+sys.path.append('./support')
+from helpers import run_solution, get_output_sample
+
+class FileSizeTest(unittest.TestCase):
+
+    def test_solution(self):
+        generated = run_solution(__file__)
+        expected  = get_output_sample(__file__)
+        self.assertEqual(expected, generated)
