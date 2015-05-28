cf2cc40b83cf06999d4f54e91eb60468d8c3b554
thiagoalessio <thiagoalessio@me.com>
2015-05-28 12:01:17 +0200

    file-size solution implemented in js

---
+++ easy/file-size/test.js
@@ -0,0 +1,15 @@
+var assert          = require('assert');
+var helpers         = require('../../support/helpers');
+var runSolution     = helpers.runSolution;
+var getOutputSample = helpers.getOutputSample;
+
+describe('file-size', function () {
+    it('should return the size of a given file', function (done) {
+        runSolution(__dirname, function (generated) {
+            getOutputSample(__dirname, function (expected) {
+                assert.equal(expected, generated);
+                done();
+            });
+        });
+    });
+});
